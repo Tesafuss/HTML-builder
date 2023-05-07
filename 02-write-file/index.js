85f55c32973ab80d76f88cecd29d07d7a1ea75e5
Dmitriy Suhin <D.Suhin94@gmail.com>
2023-05-07 15:31:38 +0300

    feat: decide 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const { stdout } = process;
+const fs = require("fs");
+const path = require("path");
+const { stdin } = require("process");
+
+const fileAdress = path.join(__dirname, "text.txt");
+
+fs.writeFile(fileAdress, "", (err) => {
+    if(err) throw err;
+  }
+)
+
+stdout.write("Пожалуйста, введите текст\n");
+stdin.on("data", data => {
+  fs.appendFile(fileAdress, data, (err) => {
+    if(err) throw err;
+  })
+  if(data.toString().slice(0, 4) === "exit"){
+    process.on('exit', () => stdout.write('Файл, закрыт. Удачи!'));
+    process.exit();
+  }
+})
+
